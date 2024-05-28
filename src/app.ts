3389317c3fe04fa952c06fc805aea024f97ec4c4
saiket-das <saiketdas02@gmail.com>
2024-05-28 08:37:51 +0600

    Dev: create index route

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import { StudentRoutes } from './app/modules/student/student.route';
 import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 
 const app: Application = express();
 
@@ -18,8 +19,7 @@ app.use(express.json());
 app.use(cors());
 
 // routes
-app.use('/api/v1/users', UserRoutes);
-app.use('/api/v1/students', StudentRoutes);
+app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Server is running!');
