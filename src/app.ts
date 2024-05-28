69b5f4cafbdb18103605e660ce7eef6be7fb0144
saiket-das <saiketdas02@gmail.com>
2024-05-28 08:18:26 +0600

    Dev: create not found route

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import cors from 'cors';
 import { StudentRoutes } from './app/modules/student/student.route';
 import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
 
 const app: Application = express();
 
@@ -25,5 +26,8 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.use(globalErrorHandler);
+
+// Not found (404) route
+app.use(notFound);
 
 export default app;
