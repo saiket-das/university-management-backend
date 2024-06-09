47942485ba3f8bcaf7bed5c08f1f926cbd2055b0
saiket-das <saiketdas02@gmail.com>
2024-06-09 21:05:07 +0600

    Dev: validate start & end time before create a new offered course

--- src/app/modules/offeredCourse/offeredCourse.validation.ts
+++ src/app/modules/offeredCourse/offeredCourse.validation.ts
@@ -14,34 +14,55 @@ const timeStringSchema = z.string().refine(
 
 // Validation schema to create a new offered course
 const createOfferedCourseValidationSchema = z.object({
-  body: z.object({
-    semesterRegistration: z.string(),
-    academicFaculty: z.string(),
-    academicDepartment: z.string(),
-    course: z.string(),
-    faculty: z.string(),
-    section: z.string(),
-    maxCapacity: z.number(),
-    days: z.array(z.enum([...(Days as [string, ...string[]])])),
-    startTime: timeStringSchema,
-    endTime: timeStringSchema,
-  }),
+  body: z
+    .object({
+      semesterRegistration: z.string(),
+      academicFaculty: z.string(),
+      academicDepartment: z.string(),
+      course: z.string(),
+      faculty: z.string(),
+      section: z.string(),
+      maxCapacity: z.number(),
+      days: z.array(z.enum([...(Days as [string, ...string[]])])),
+      startTime: timeStringSchema,
+      endTime: timeStringSchema,
+    })
+    .refine(
+      (body) => {
+        // startTime : 10:30  => 1970-01-01T10:30
+        // endTime : 12:30  =>  1970-01-01T12:30
+
+        const start = new Date(`1970-01-01T${body.startTime}:00`);
+        const end = new Date(`1970-01-01T${body.endTime}:00`);
+        return end > start;
+      },
+      { message: 'Start time should be before End time!' },
+    ),
 });
 
 // Validation schema to update an exisiting offered course
 const updateOfferedCourseValidationSchema = z.object({
-  body: z.object({
-    semesterRegistration: z.string(),
-    academicFaculty: z.string(),
-    academicDepartment: z.string(),
-    course: z.string(),
-    faculty: z.string().optional(),
-    section: z.string(),
-    maxCapacity: z.number().optional(),
-    days: z.enum([...(Days as [string, ...string[]])]).optional(),
-    startTime: z.string().datetime().optional(),
-    endTime: z.string().datetime().optional(),
-  }),
+  body: z
+    .object({
+      semesterRegistration: z.string(),
+      academicFaculty: z.string(),
+      academicDepartment: z.string(),
+      course: z.string(),
+      faculty: z.string().optional(),
+      section: z.string(),
+      maxCapacity: z.number().optional(),
+      days: z.enum([...(Days as [string, ...string[]])]).optional(),
+      startTime: z.string().datetime().optional(),
+      endTime: z.string().datetime().optional(),
+    })
+    .refine(
+      (body) => {
+        const start = new Date(`1970-01-01T${body.startTime}:00`);
+        const end = new Date(`1970-01-01T${body.endTime}:00`);
+        return end > start;
+      },
+      { message: 'Start time should be before End time!' },
+    ),
 });
 
 export const OfferedCourseValidations = {
