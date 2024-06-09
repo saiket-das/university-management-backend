2fce579b8f8dade7d84b552e9d422c083bdcbad3
saiket-das <saiketdas02@gmail.com>
2024-06-09 20:00:16 +0600

    Dev: update offered course validation schema

--- src/app/modules/offeredCourse/offeredCourse.validation.ts
+++ src/app/modules/offeredCourse/offeredCourse.validation.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 import { Days } from './offeredCourse.constant';
 
+// Validation schema to create a new offered course
 const createOfferedCourseValidationSchema = z.object({
   body: z.object({
     academicSemester: z.string(),
@@ -17,6 +18,24 @@ const createOfferedCourseValidationSchema = z.object({
   }),
 });
 
-export const SemesterRegistrationValidations = {
+// Validation schema to update an exisiting offered course
+const updateOfferedCourseValidationSchema = z.object({
+  body: z.object({
+    academicSemester: z.string(),
+    semesterRegistration: z.string(),
+    academicFaculty: z.string(),
+    academicDepartment: z.string(),
+    course: z.string(),
+    faculty: z.string(),
+    section: z.string(),
+    maxCapacity: z.number(),
+    days: z.enum([...(Days as [string, ...string[]])]),
+    startTime: z.string().datetime(),
+    endTime: z.string().datetime(),
+  }),
+});
+
+export const OfferedCourseValidations = {
   createOfferedCourseValidationSchema,
+  updateOfferedCourseValidationSchema,
 };
