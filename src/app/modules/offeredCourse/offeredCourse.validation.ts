b89c8d7477371b1d49e632dc1d07afcf70263c1e
saiket-das <saiketdas02@gmail.com>
2024-06-09 20:59:49 +0600

    Dev: validate time format before create a new offered course

--- src/app/modules/offeredCourse/offeredCourse.validation.ts
+++ src/app/modules/offeredCourse/offeredCourse.validation.ts
@@ -1,6 +1,17 @@
 import { z } from 'zod';
 import { Days } from './offeredCourse.constant';
 
+// Validate time format (hour:mintue --> 10:30)
+const timeStringSchema = z.string().refine(
+  (time) => {
+    const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/; // 00-09 10-19 20-23
+    return regex.test(time);
+  },
+  {
+    message: 'Invalid time format. Expected "HH:MM" in 24 hours format',
+  },
+);
+
 // Validation schema to create a new offered course
 const createOfferedCourseValidationSchema = z.object({
   body: z.object({
@@ -12,8 +23,8 @@ const createOfferedCourseValidationSchema = z.object({
     section: z.string(),
     maxCapacity: z.number(),
     days: z.array(z.enum([...(Days as [string, ...string[]])])),
-    startTime: z.string(),
-    endTime: z.string(),
+    startTime: timeStringSchema,
+    endTime: timeStringSchema,
   }),
 });
 
