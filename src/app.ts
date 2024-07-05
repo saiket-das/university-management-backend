d09e20b99b8ecab7dda3bbd940426f25ddc8d359
saiket-das <saiketdas02@gmail.com>
2024-07-06 04:16:27 +0600

    Check decoded JWT

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,8 @@ const app: Application = express();
 // parsers
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+// app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
 
 // routes
 app.use('/api/v1', router);
